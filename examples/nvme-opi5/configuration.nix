de953950ebe80eca990cc69e82c682cb451fc2fa
ioitiki <andymalkin@hotmail.com>
2026-02-27 15:18:55 -0800

    Add XFCE desktop environment with LightDM
    Enables xserver, XFCE, LightDM display manager, adds firefox
    and xfce4-terminal, and adds video/audio/input groups to user.

--- examples/nvme-opi5/configuration.nix
+++ examples/nvme-opi5/configuration.nix
@@ -12,7 +12,15 @@
     mtdutils
     i2c-tools
     minicom
+    firefox
+    xfce.xfce4-terminal
   ];
+
+  services.xserver = {
+    enable = true;
+    desktopManager.xfce.enable = true;
+    displayManager.lightdm.enable = true;
+  };
 
   services.openssh = {
     enable = true;
@@ -27,7 +35,7 @@
     initialPassword = "changeme";
     isNormalUser = true;
     home = "/home/andy";
-    extraGroups = ["users" "wheel"];
+    extraGroups = ["users" "wheel" "video" "audio" "input"];
   };
 
   system.stateVersion = "26.05";
